bb668cec41881f633dc61123d14806f262ba71dc
Anest <103149273+Anest2009@users.noreply.github.com>
2024-12-26 20:52:00 +0100

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 export default function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Navba />
+      <Navbar />
       <main>
         <Hero />
         <Gallery />
